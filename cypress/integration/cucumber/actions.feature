980523e67a0f710adb52c45eb508b93a04d9cebe
alexandrerousselledecath <alexandre.rousselle@decathlon.com>
2020-06-30 10:32:14 +0200

    feat(cucumber): first test + ci implem

---
+++ cypress/integration/cucumber/actions.feature
@@ -0,0 +1,8 @@
+@core
+Feature: Actions
+
+  @severity-high
+  Scenario: .type() - type into a DOM element
+
+    Given a user visits actions page
+    When the user types into DOM elements
---
+++ cypress/support/step_definitions/actions.feature.js
@@ -0,0 +1,27 @@
+import { Given, When, Then } from 'cypress-cucumber-preprocessor/steps';
+
+// Given
+Given('a user visits actions page', () => { cy.visit('https://example.cypress.io/commands/actions') });
+When('the user types into DOM elements', () => { 
+	cy.get('.action-email')
+		.type('fake@email.com').should('have.value', 'fake@email.com')
+
+		// .type() with special character sequences
+		.type('{leftarrow}{rightarrow}{uparrow}{downarrow}')
+		.type('{del}{selectall}{backspace}')
+
+		// .type() with key modifiers
+		.type('{alt}{option}') //these are equivalent
+		.type('{ctrl}{control}') //these are equivalent
+		.type('{meta}{command}{cmd}') //these are equivalent
+		.type('{shift}')
+
+		// Delay each keypress by 0.1 sec
+		.type('slow.typing@email.com', { delay: 100 })
+		.should('have.value', 'slow.typing@email.com');
+	cy.get('.action-disabled')
+		// Ignore error checking prior to type
+		// like whether the input is visible or disabled
+		.type('disabled error checking', { force: true })
+		.should('have.value', 'disabled error checking');
+});
